b2a4d850e3cb5dfb7a74e9482ed62a8c60f3a8c2
elliotmai <elliotmai@users.noreply.github.com>
2024-07-07 00:36:01 +0000

    deploy: 44d6444925965f24b1c13300a08ab90913537207

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyApq0Wj12n2Gn8tjD0HJ-MsR3ON8hbh7QQ",
+  authDomain: "aerologger-ec6a6.firebaseapp.com",
+  projectId: "aerologger-ec6a6",
+  storageBucket: "aerologger-ec6a6.appspot.com",
+  messagingSenderId: "1080261936802",
+  appId: "1:1080261936802:web:59543e21bc5c2a4faab9fb"
 };
 firebase.initializeApp(firebaseConfig);
